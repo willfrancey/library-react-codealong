00c6815366302d433752525488ddef658c79545e
willfrancey <125171714+willfrancey@users.noreply.github.com>
2024-04-04 15:15:04 +1000

    first commit nav

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Nav from "./components/Nav";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import reportWebVitals from './reportWebVitals';
+import './index.css'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars, faShoppingCart, faTimes } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBars, faShoppingCart, faTimes)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
